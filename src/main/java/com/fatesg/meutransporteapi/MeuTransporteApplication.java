b573f871bd06b64a3d48b9669c21f673b4290145
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-12 20:19:43 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/fatesg/meutransporteapi/MeuTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.fatesg.meutransporteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTransporteApplication.class, args);
+	}
+
+}
