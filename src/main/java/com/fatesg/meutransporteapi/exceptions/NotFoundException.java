6dd1d60e0a861a06ff814ba2d96bcaaf9420cbda
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-26 21:29:49 -0300

    Criado classes para tratamento de exceções e validações.

---
+++ src/main/java/com/fatesg/meutransporteapi/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.fatesg.meutransporteapi.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException(String msg) {
+        super(msg);
+    }
+
+}
