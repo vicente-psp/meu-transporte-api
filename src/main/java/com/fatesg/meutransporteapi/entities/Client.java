ad4cbdeeba7faabe2e23062bd9b807275be4f692
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-19 15:58:31 -0300

    Ajuste/organização nos imports, ajuste/correção na validação dos atributos e incluído atributos.

--- src/main/java/com/fatesg/meutransporteapi/entities/Client.java
+++ src/main/java/com/fatesg/meutransporteapi/entities/Client.java
@@ -1,16 +1,10 @@
 package com.fatesg.meutransporteapi.entities;
 
+import java.util.Date;
+import javax.validation.constraints.*;
+import javax.persistence.*;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.SequenceGenerator;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -21,11 +15,29 @@ import javax.validation.constraints.Size;
 public class Client extends User {
 
     @NotBlank(message = "Razão social é obrigatório")
-    @Size(min = 3, message = "Razão social deve ter no mínimo 3 caracteres")
+    @Size(min = 3, message = "Razão social deve ter de 3 a 150 caracteres")
     @Column(name = "social_name", length = 150, nullable = false)
     private String socialName;
 
+    @Size(max = 75, message = "Nome fantasia deve ter no máximo 75 caracteres")
     @Column(name = "name_fantasy", length = 75)
     private String nameFantasy;
+
+    @NotNull(message = "CNPJ é obrigatório")
+    @Size(max = 14, message = "CNPJ deve ter no máximo 14 caracteres")
+    @Column(length = 14, nullable = false)
+    private String cnpj;
+
+    @Size(max = 20, message = "Nome deve ter no máximo 20 caracteres")
+    @Column(length = 20)
+    private String phone;
+
+    @Column(name = "initial_date", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date initialDate;
+
+    @Column(name = "final_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date finalDate;
     
 }
