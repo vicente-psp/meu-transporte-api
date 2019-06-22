abf9dd4c32afc5e29c7cb9d2ffcf0d779442f5fb
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-22 19:50:37 -0300

    Expondo autentication manager como Bean.

--- src/main/java/com/fatesg/meutransporteapi/security/SecurityConfig.java
+++ src/main/java/com/fatesg/meutransporteapi/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.fatesg.meutransporteapi.security;
 
 import com.fatesg.meutransporteapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(customPasswordEconder);
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
