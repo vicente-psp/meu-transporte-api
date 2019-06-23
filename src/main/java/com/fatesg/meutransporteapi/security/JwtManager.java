dcfbb9b9a32cabfaf05c91f92baa1a1006dd1b50
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-22 21:27:16 -0300

    Criado classe para gerenciar jwt.

---
+++ src/main/java/com/fatesg/meutransporteapi/security/JwtManager.java
@@ -0,0 +1,25 @@
+package com.fatesg.meutransporteapi.security;
+
+import com.fatesg.meutransporteapi.constant.SecurityConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Component
+public class JwtManager {
+
+    public String createToken(String userName, List<String> roles) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, SecurityConstants.JWT_EXP_DAYS);
+        String jwt = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(calendar.getTime())
+                .claim(SecurityConstants.JWT_ROLE_KEY, roles)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.API_KEY.getBytes())
+                .compact();
+        return jwt;
+    }
+}
