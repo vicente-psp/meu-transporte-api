82f5b94366f91fa15af0bdb648d0d33a95eec1dd
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-23 15:46:00 -0300

    Adicionado constante JWT_INVALID_MSG.

--- src/main/java/com/fatesg/meutransporteapi/constant/SecurityConstants.java
+++ src/main/java/com/fatesg/meutransporteapi/constant/SecurityConstants.java
@@ -6,5 +6,6 @@ public class SecurityConstants {
     public static final String API_KEY = "meu-transporte";
     public static final String JWT_PROVIDER = "Bearer";
     public static final String JWT_ROLE_KEY = "role";
-    
+    public static final String JWT_INVALID_MSG = "Invalid Jwt token";
+
 }
