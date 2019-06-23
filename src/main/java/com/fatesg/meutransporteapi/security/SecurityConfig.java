39e7d5dd6ccb8ea2aac2b36a67787c668e878bba
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-22 23:47:44 -0300

    Adicionado método para ignorar proteção da rota /drivers/login.

--- src/main/java/com/fatesg/meutransporteapi/security/SecurityConfig.java
+++ src/main/java/com/fatesg/meutransporteapi/security/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.fatesg.meutransporteapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -29,4 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/drivers/login");
+    }
 }
