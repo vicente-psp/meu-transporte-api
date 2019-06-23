e2946c03308bab8d6143758110f4ed6056897b62
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-23 00:40:59 -0300

    Criado método handleBadCredentialsException para personalizar exceções do tipo BadCredentialsException.

--- src/main/java/com/fatesg/meutransporteapi/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fatesg/meutransporteapi/controllers/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package com.fatesg.meutransporteapi.controllers.exceptions;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,12 +20,19 @@ import com.fatesg.meutransporteapi.exceptions.NotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiError> handlerNotFoundException(NotFoundException e) {
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), e.getMessage(), new Date());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex) {
+        ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders httpHeaders, HttpStatus httpStatus, WebRequest webRequest) {
         List<String> errors = new ArrayList<>();
