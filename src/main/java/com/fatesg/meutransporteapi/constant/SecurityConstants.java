1f4f5b3fecc5dd3907375d8099adc1e3c1693efc
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-22 21:28:11 -0300

    Criado classe de constantes.

---
+++ src/main/java/com/fatesg/meutransporteapi/constant/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.fatesg.meutransporteapi.constant;
+
+public class SecurityConstants {
+
+    public static final int JWT_EXP_DAYS = 5;
+    public static final String API_KEY = "meu-transporte";
+    public static final String JWT_PROVIDER = "Bearer";
+    public static final String JWT_ROLE_KEY = "role";
+    
+}
